d25a6c7f9945a864b276d9629bdcf8b91ce286ec
Jp Keys <jpmusik29@gmail.com>
2024-06-03 17:44:14 -0400

    Installing Chakra

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import SearchBar from './components/SearchBar'
 import WeatherCard from './components/WeatherCard'
+import { Box } from '@chakra-ui/react';
 
 function App() {
   
   return (
-    <>
+    <Box bg='#80daea'>
     <Header />
     <SearchBar />
     <MainContent>
       <WeatherCard />
       <Forecast />
-    </MainContent>
-      
-    </>
+    </MainContent>  
+    </Box>
   )
 }
 
